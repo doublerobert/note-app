0cb2b514051c6e8352c508b4caa7ae94a70785ee
Robert Robert <doublerobert001@gmail.com>
2026-03-21 13:12:59 +0100

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+
+const port = process.env.PORT;
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => {
+  console.log("Mongoose connected successfully");
+  app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+  });
+});
+
